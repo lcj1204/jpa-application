0aa558735dba52e8a7ab06e1f2aeca5b0f020231
lcj1204 <dlckdwo1204@naver.com>
2023-03-13 20:19:32 +0900

    [Feat]: 주문 취소, 재고수량초과 테스트 구현

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockExceptioin;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,7 +33,7 @@ public class OrderServiceTest {
     @Autowired OrderRepository orderRepository;
 
     @Test
-    @Rollback(value = false)
+//    @Rollback(value = false)
     public void 주문_테스트() throws Exception {
         //given
         Member member = createMember();
@@ -49,6 +50,39 @@ public class OrderServiceTest {
         assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
         assertEquals("주문 가격은 가격 * 수량이다.", 10000 * 2, getOrder.getTotalPrice());
         assertEquals("주문 수량만크 재고가 줄어야 한다.", 8, item.getStockQuantity());
+    }
+
+    @Test(expected = NotEnoughStockExceptioin.class)
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10);
+
+        //when
+        int orderCount = 11;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10);
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //when
+        orderService.orderCancel(orderId);
+        Order getOrder = orderRepository.findOne(orderId);
+
+        //then
+        assertEquals("주문 취소시 상태는 CANCEL dlek.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
+
     }
 
     private Member createMember() {
