23524029d7ab7e14556b8eb0115b3a17605a3153
lcj1204 <dlckdwo1204@naver.com>
2023-03-13 18:35:26 +0900

    [Feat]: 주문 서비스 구현

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +16,48 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
 
-    // 주문
+    /**
+     * 주문
+     */
     @Transactional
-    public void order(Long ) {
+    public Long order(Long memberId, Long itemId, int count) {
 
+        // 멤버, 아이템 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송 정보
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장?
+        orderRepository.save(order);
+        return order.getId();
     }
 
-    // 주문 내역 조회
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void orderCancel(Long orderId) {
 
-    // 주문 취소
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+    }
+
+    /**
+     * 주문 검색
+     */
 }
