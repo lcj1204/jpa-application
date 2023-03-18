d8c9e9d50d4727c239dcfd40ed94ad8ac971b2d8
lcj1204 <dlckdwo1204@naver.com>
2023-03-18 18:07:15 +0900

    [Refactor]: Address 도메인에 빌더 생성자 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,20 +1,21 @@
 package jpabook.jpashop.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Address {
     private String city;
     private String street;
     private String zipcode;
 
-    protected Address() { // 기본 생성자?
-    }
-
+    @Builder
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
